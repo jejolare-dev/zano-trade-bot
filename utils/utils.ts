d679942f9b34f2fc0083f1898628a08eb56f6891
jejolare <jejolare@gmail.com>
2025-09-04 18:17:15 +0700

    remove debug delay

--- utils/utils.ts
+++ utils/utils.ts
@@ -263,7 +263,7 @@ async function _onOrdersNotify(authToken: string, observedOrderId: number, pairD
 
 		logger.debug("tx data");
 		logger.debug(txData);
-		await new Promise(resolve => setTimeout(resolve, 20000));
+		// await new Promise(resolve => setTimeout(resolve, 20000));
 		const success = await _processTransaction(matchedApplyTip.hex_raw_proposal, matchedApplyTip.id, authToken, txData);
 
 		if (success) {
