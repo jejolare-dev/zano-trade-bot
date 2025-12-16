08cb144a2c16b356edbd6f6c3d0a784f1077d758
Andrew Besedin <andrewprog97@gmail.com>
2025-12-16 20:23:48 +0300

    add: add override no-extraneous-dependencies rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,16 @@
 module.exports = {
-  // Or 'jejolare/backend' for Node.js app
-  extends: ['jejolare/backend'],
+    // Or 'jejolare/backend' for Node.js app
+    extends: ['jejolare/backend'],
 
-  // This is needed only if you use TypeScript
-  settings: {
-    'import/resolver': {
-      typescript: {
-        project: './tsconfig.json',
-      },
+    // This is needed only if you use TypeScript
+    settings: {
+        'import/resolver': {
+            typescript: {
+                project: './tsconfig.json',
+            },
+        },
     },
-  },
+    rules: {
+        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    },
 };
